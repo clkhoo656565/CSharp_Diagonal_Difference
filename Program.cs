8902f130395dd54b0ffc6046e69ed928fb7752bf
CHEE LONG KHOO <khoo@dxc.com>
2022-03-26 12:45:46 +0800

    Diagonal difference

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp_Diagonal_difference
+{
+   class program
+   {
+       public static void Main(string[] args)
+       {
+           List<List<int>> arr = new List<List<int>>
+           {
+               new List<int>(){11,2,4},
+               new List<int>(){4,5,6},
+               new List<int>(){10,8,-12}
+           };
+
+            //Foreach loop to see the matrix
+            // foreach(var item in arr)
+            // {
+            //     foreach(var item2 in item)
+            //     {
+            //         Console.WriteLine(item2);
+            //     }
+            // }
+
+
+            int rowCount = arr[0].Count - 1;
+            int right = 0;
+            int left = 0;
+            int result = 0;
+
+            for(int i = 0; i < arr[0].Count; i++)
+            {
+                Console.WriteLine("index {0}", i);
+                Console.WriteLine("item {0}",arr[i][i]);
+                Console.WriteLine("item_b {0}",arr[rowCount - i][i]);
+
+                right += arr[i][i];
+                left += arr[rowCount - i][i];
+
+                result = Math.Abs(right - left);
+                Console.WriteLine("result {0}", result);
+            }
+            
+
+           
+       }
+   }
+}
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # CSharp_Diagonal_Difference
 Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+###Function description
+
+Complete the diagonalDifference function in the editor below.
+
+diagonalDifference takes the following parameter:
+
+- int arr[n][m]: an array of integers
+#Return
+
+- int: the absolute diagonal difference
+#Input Format
+
+The first line contains a single integer, , the number of rows and columns in the square matrix .
+Each of the next  lines describes a row,arr[i], and consists of  space-separated integers arr[i][j].
+
+#Output Format
+
+Return the absolute difference between the sums of the matrix's two diagonals as a single integer.
+
+#Sample Input
+
+3
+11 2 4
+4 5 6
+10 8 -12
+#Sample Output
+
+15
+
+#Explanation
+
+The primary diagonal is:
+
+11
+   5
+     -12
+Sum across the primary diagonal: 11 + 5 - 12 = 4
+
+The secondary diagonal is:
+
+     4
+   5
+10
+Sum across the secondary diagonal: 4 + 5 + 10 = 19
+Difference: |4 - 19| = 15
+
+Note: |x| is the absolute value of x
